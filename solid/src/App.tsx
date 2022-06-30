153c555946a7d031275e148853e302aa933dd1ff
Rohit Sah <sahrohit9586@gmail.com>
2022-06-30 11:25:00 +0545

    solid init

---
+++ solid/src/App.tsx
@@ -0,0 +1,12 @@
+import type { Component } from "solid-js";
+
+const App: Component = () => {
+	return (
+		<>
+			<h1>Hello from App</h1>
+			<p className="text-green-500">This is the body</p>
+		</>
+	);
+};
+
+export default App;

8c3df7124d83f8b3f390a9a56e2c7988945fd78b
Rohit Sah <sahrohit9586@gmail.com>
2022-07-01 00:29:54 +0545

    Fixed Solid Eslint Issue

--- solid/src/App.tsx
+++ solid/src/App.tsx
@@ -4,7 +4,7 @@ const App: Component = () => {
 	return (
 		<>
 			<h1>Hello from App</h1>
-			<p className="text-green-500">This is the body</p>
+			<p class="text-green-500">This is the body</p>
 		</>
 	);
 };
